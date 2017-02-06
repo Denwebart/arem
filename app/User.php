a6eba2b4e390bebbf1b2d34487060afed2b27ca8
denwebart <denwebart@github.com>
2017-02-06 19:57:44 +0200

    Миграция: изменение таблицы users.

--- app/User.php
+++ app/User.php
@@ -8,14 +8,41 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
+	
+	/**
+	 * Role
+	 */
+	const ROLE_ADMIN = 1;
+	const ROLE_MODERATOR = 2;
+	const ROLE_USER = 3;
+	
+	public static $roles = [
+		self::ROLE_ADMIN => 'Администратор',
+		self::ROLE_MODERATOR => 'Модератор',
+		self::ROLE_USER => 'Пользователь',
+	];
+	
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+	    'role',
+	    'alias',
+	    'login',
+	    'email',
+	    'avatar',
+	    'firstname',
+	    'lastname',
+	    'city',
+	    'country',
+	    'car_brand',
+	    'car_model',
+	    'profession',
+	    'description',
+	    'sex',
+	    'birthday',
     ];
 
     /**
---
+++ database/migrations/2017_02_06_161702_change_users_table.php
@@ -0,0 +1,70 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+	    Schema::table('users', function (Blueprint $table) {
+		    $table->renameColumn('name', 'login');
+		    $table->tinyInteger('role')->default(3)->after('id');
+		    $table->string('alias', 150)->after('role');
+		    $table->boolean('is_active')->default(0)->after('email');
+		    $table->boolean('is_banned')->default(0)->after('is_active');
+		    $table->boolean('is_online')->default(0)->after('is_banned');
+		    $table->timestamp('last_activity')->nullable()->after('is_online');
+		    $table->string('avatar')->nullable()->after('last_activity');
+		    $table->string('firstname', 100)->nullable()->after('avatar');
+		    $table->string('lastname', 100)->nullable()->after('firstname');
+		    $table->string('city', 150)->nullable()->after('lastname');
+		    $table->string('country', 150)->nullable()->after('city');
+		    $table->integer('points')->default(0)->after('country');
+		    $table->string('car_brand', 150)->nullable()->after('points');
+		    $table->string('car_model', 150)->nullable()->after('car_brand');
+		    $table->string('profession', 150)->nullable()->after('car_model');
+		    $table->text('description')->after('profession');
+		    $table->tinyInteger('sex')->default(0)->after('description');
+		    $table->timestamp('birthday')->nullable()->after('sex');
+		    $table->string('activation_code');
+	    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+	    Schema::table('users', function (Blueprint $table) {
+		    $table->renameColumn('login', 'name');
+		    $table->dropColumn('alias');
+		    $table->dropColumn('role');
+		    $table->dropColumn('is_active');
+		    $table->dropColumn('is_banned');
+		    $table->dropColumn('is_online');
+		    $table->dropColumn('last_activity');
+		    $table->dropColumn('avatar');
+		    $table->dropColumn('firstname');
+		    $table->dropColumn('lastname');
+		    $table->dropColumn('city');
+		    $table->dropColumn('country');
+		    $table->dropColumn('points');
+		    $table->dropColumn('car_brand');
+		    $table->dropColumn('car_model');
+		    $table->dropColumn('profession');
+		    $table->dropColumn('description');
+		    $table->dropColumn('sex');
+		    $table->dropColumn('birthday');
+		    $table->dropColumn('activation_code');
+	    });
+    }
+}
